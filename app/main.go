6530b82edf026a80936939fd9ad4c802b159170c
Youngsun Yoo <youngsun123q@gmail.com>
2025-06-02 23:46:59 -0600

    add another fix for EOF

--- app/main.go
+++ app/main.go
@@ -34,7 +34,7 @@ func main() {
 	fileContents := string(rawFileContents)
 	l := lexer.New(fileContents, filename)
 	hasLexicalErrors := false
-	for !l.IsAtEnd() { 
+	for { 
 		tok := l.NextToken()
 		if tok.Type == token.ERROR {
 			fmt.Fprintf(os.Stderr, "[line %v] Error: Unexpected character: %s\n", tok.Position.Line, tok.Lexeme)
@@ -42,6 +42,9 @@ func main() {
 			continue
 		}
 		fmt.Println(tok.String())
+		if tok.Type == token.EOF {
+			break
+		}
 	}
 	if hasLexicalErrors {
 		os.Exit(65)
